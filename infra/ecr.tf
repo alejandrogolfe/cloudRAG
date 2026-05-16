8fcca19ccd4afef257437684830a362f8ce658b2
Alejandro Golfe San Martin <algolsan@upv.edu.es>
2026-05-16 21:31:35 +0200

    updated ecr to remove ecr non-empty repos

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -5,6 +5,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = "cloudrag-${var.environment}"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true  # allows destroy even when images exist
 
   image_scanning_configuration {
     scan_on_push = true
